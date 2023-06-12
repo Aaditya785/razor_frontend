5d8736f71db83d20c1c073b5392e75ee273dc0fe
Aaditya785 <79626304+Aaditya785@users.noreply.github.com>
2023-06-12 12:03:05 +0530

    bug fixed

--- src/Home.jsx
+++ src/Home.jsx
@@ -32,7 +32,7 @@ const Home = () => {
                 prefill: {
                     name: "Gaurav Kumar",
                     email: "gaurav.kumar@example.com",
-                    contact: "9000090000"
+                    contact: "7206317881"
                 },
                 notes: {
                     address: "Razorpay Corporate Office"
